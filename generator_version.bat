79fc2bdd04d19357093c270e0ecf37d1bdb61a7b
makatt <loshamaks@gmail.com>
2025-03-10 09:36:40 +0300

    create gen

---
+++ generator_version.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set n=10
+set list=
+
+for /l %%i in (0,1,%n%) do (
+    set list=!list! %%i
+)
+
+echo List created with generator-like approach: %list%
+pause
